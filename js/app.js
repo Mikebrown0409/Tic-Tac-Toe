5eb6723afe698e6e2ae51c8fcd4efd45b82d2bd5
Mikebrown0409 <mikebrown0409@gmail.com>
2025-04-25 10:48:05 -0700

    Tic Tac Toe Full Game

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
 // look-up data structure
 const COLORS = {
-    '1': 'purple',
+    '1': 'blue',
     '-1': 'orange',
     'null': 'white',
   };
@@ -19,7 +19,7 @@ const COLORS = {
   const squareEls = [...document.querySelectorAll('.board > div')]
 
   /*----- event listeners -----*/
-//   document.querySelector('.board').addEventListener('click', handlePick);
+  document.querySelector('.board').addEventListener('click', handlePick);
   playAgainBtn.addEventListener('click', init);
 
   
@@ -39,6 +39,69 @@ const COLORS = {
     render();
   }
   
+
+
+  function handlePick(evt) {
+    const id = evt.target.id; 
+    if (!id.startsWith('r') || winner) return;
+    const row = parseInt(id[1]);
+    const col = parseInt(id[3]); 
+    if (board[row][col] !== null) return; 
+    board[row][col] = turn; 
+    winner = getWinner();
+    turn *= -1;
+    render();
+   }
+
+   function getWinner() {
+    return checkRows() || checkCols() || checkDiag() || checkTie();
+   }
+
+   function checkRows () {
+    for (let rowIdx = 0; rowIdx < 3; rowIdx++) {
+        if (board[rowIdx][0] !== null &&
+            board[rowIdx][0] === board[rowIdx][1] &&
+            board[rowIdx][0] === board[rowIdx][2]) {
+                return board[rowIdx][0];
+        }
+    }
+    return null;
+   }
+
+   function checkCols() {
+    for (let colIdx = 0; colIdx < 3; colIdx++) {
+        if (board[0][colIdx] !== null &&
+            board[0][colIdx] === board[1][colIdx] &&
+            board[0][colIdx] === board[2][colIdx]) {
+                return board[0][colIdx];
+        }
+    }
+    return null;
+   }
+
+   function checkDiag() {
+    //reg diag
+    if (board[0][0] !== null &&
+        board[0][0] === board[1][1] &&
+        board[0][0] === board[2][2]){
+            return board[0][0];
+    }
+    //reverse diag
+    if (board[0][2] !== null &&
+        board[0][2] === board[1][1] &&
+        board[0][2] === board[2][0]){
+            return board[0][2];
+    }
+    return null;
+   }
+
+   function checkTie() {
+    if (board.flat().every(cell => cell !== null)) {
+      return 'Tie';
+    }
+    return null;
+  }
+
 
   function render() {
     renderBoard();
